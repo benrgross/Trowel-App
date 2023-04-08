461295942a04540f36f5b8db35b79d8cbfa97a9d
benrgross <benrgross@gmail.com>
2023-04-08 17:21:25 -0400

    add prisma

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap'
-        />
+        <link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap' />
       </Head>
       <Layout>
         <main>
