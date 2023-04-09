1838606f3f607dea75a5c53b6fb7a8d3c34fae6c
benrgross <benrgross@gmail.com>
2023-04-09 00:45:50 -0400

    Change name of next auth secret

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     JWT_SECRET: process.env.JWT_SECRET,
-    NEXT_AUTH_SECRET: process.env.NEXT_AUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXT_AUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
 };
