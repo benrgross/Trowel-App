e964f0fc694a3c5a5f0ada6110245d7507789403
benrgross <benrgross@gmail.com>
2023-04-08 23:16:34 -0400

    Adjust import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/layout';
+import Layout from '../components/layout';
 import { useSession } from 'next-auth/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
